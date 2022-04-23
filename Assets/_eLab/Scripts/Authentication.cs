09f2e4181df31d2f354236fbbd0661fd121eefbd
DESKTOP-ECO4G8G\user <kdr.mauladan@gmail.com>
2022-04-23 16:23:54 +0700

    Added database and login API

---
+++ Assets/_eLab/Scripts/Authentication.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Networking;
+
+public class Authentication : MonoBehaviour
+{
+    const string rootUrl = "http://localhost/elab/";
+
+    [Header("Authentication")]
+    public TMP_InputField loginEmail;
+    public TMP_InputField loginPassword;
+
+    public TMP_InputField registerUsername;
+    public TMP_InputField registerEmail;
+    public TMP_InputField registerPassword;
+    public TMP_InputField registerCPassword;
+
+    public void OnLoginButtonClicked()
+    {
+        StartCoroutine(Login());
+    }
+
+    public void OnRegisterButtonClicked()
+    {
+
+    }
+
+	IEnumerator Login()
+	{
+		WWWForm	form = new WWWForm();
+
+		form.AddField("email", loginEmail.text);
+		form.AddField("password", loginPassword.text);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(rootUrl + "login.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                string responseText = www.downloadHandler.text;
+                if (responseText.StartsWith("GRANTED"))
+                {
+                    string[] dataChunks = responseText.Split('|');
+                    foreach (var data in dataChunks)
+                    {
+                        Debug.Log(data);
+                    }
+                }
+                else Debug.Log(responseText);
+            }
+        }
+	}
+}
