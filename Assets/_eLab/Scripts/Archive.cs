fc3c55bfc6c6ae05a06eedce6d42bd0e3d2c0b06
DudSukarya <dudikajus7@gmail.com>
2022-06-23 00:49:09 +0700

    Added create archive feature

--- Assets/_eLab/Scripts/Archive.cs
+++ Assets/_eLab/Scripts/Archive.cs
@@ -6,9 +6,32 @@ using TMPro;
 
 public class Archive : MonoBehaviour
 {
+    public int id;
+
     public RawImage img;
     public TextMeshProUGUI title;
     public TextMeshProUGUI desc;
     public TextMeshProUGUI type;
     public TextMeshProUGUI author;
+    public TextMeshProUGUI date;
+
+    public GameObject optionButton;
+    public GameObject saveToggle;
+
+    private void Start()
+    {
+        if(User.Instance.userType == User.UserType.AUTHORIZED)
+        {
+            optionButton.SetActive(true);
+            saveToggle.SetActive(true);
+        }else if(User.Instance.userType == User.UserType.NORMAL)
+        {
+            saveToggle.SetActive(true);
+        }
+        else
+        {
+            optionButton.SetActive(false);
+            saveToggle.SetActive(false);
+        }
+    }
 }
